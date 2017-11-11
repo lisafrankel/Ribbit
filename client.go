964e54f689d622bb93b8434f16e1c57c9f92a4de
Lisa Frankel <lisa@frankel.tv>
2017-11-11 15:38:22 -0500

    seperated into backend and frontend, data now stored in files

---
+++ client.go
@@ -0,0 +1,174 @@
+// Project Pt. 2
+// LHF238 / N18380086
+// Client
+
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"html/template"
+	"time"
+	"encoding/json"
+	"net/url"
+	"log"
+)
+
+type post struct {
+	Username string
+	Post_data string
+	Time_id int
+}
+
+// handler for main feed page, to post and see other's posts
+func post_croak(w http.ResponseWriter, r *http.Request) {
+	userCookie, _ := r.Cookie("SessionID")
+	if userCookie == nil {
+		http.Redirect(w, r, "/login", 301)
+		return
+	} else if r.Method == http.MethodPost {
+		r.ParseForm()
+		resp, _ := http.PostForm("http://localhost:8081/main", url.Values{"post_data": {r.PostFormValue("post")},"username": {userCookie.Value}})
+		if resp.StatusCode == 512 {
+			fmt.Fprintf(w, "post greater than 100 characters") //how do receive specific error from server?
+			return
+		}
+	}
+
+	// response here is []posts - userfeed // this changes to /getFeed
+	response, err := http.PostForm("http://localhost:8081/getUserFeed", url.Values {"username": {userCookie.Value}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	dec := json.NewDecoder(response.Body)
+	var user_feed []post
+	dec_err := dec.Decode(&user_feed)
+	
+	if dec_err != nil {
+		log.Fatal(dec_err)
+	}
+
+	t := template.New("some_name")
+	t, _ = template.ParseFiles("main_feed.html")
+	t.Execute(w, user_feed)
+}
+
+
+// login handler
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("ribbit_home_page.html")
+		t.Execute(w, nil)
+	} else if r.Method == "POST" {
+		r.ParseForm()
+		resp, _ := http.PostForm("http://localhost:8081/login", url.Values{"username": {r.PostFormValue("username")}, "password":{r.PostFormValue("password")}})
+		if resp.StatusCode == 601 {
+			fmt.Fprintf(w, "User does not exist")
+		} else if resp.StatusCode == 602 {
+			fmt.Fprintf(w, "Incorrect password")
+		} else {
+			cookieValue := r.PostFormValue("username")
+			expire := time.Now().Add(1 * time.Hour)
+			userCookie := http.Cookie{Name: "SessionID", Value: cookieValue, Expires: expire}
+			http.SetCookie(w, &userCookie)
+			http.Redirect(w, r, "/main", 301)
+		}
+	}
+}
+
+// handler for adding and removing friends
+func manageFriends(w http.ResponseWriter, r *http.Request) {
+	userCookie, _ := r.Cookie("SessionID")
+	if userCookie == nil {
+		http.Redirect(w, r, "/login", 301)
+	}
+	username := userCookie.Value
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("manage_friends.html")
+		t.Execute(w, nil)
+	} else if r.Method == "POST" {
+		r.ParseForm()
+		friend := r.PostFormValue("username")
+		resp, _ := http.PostForm("http://localhost:8081/manage", url.Values{"username": {username}, "friend": {friend}, "choice": {r.Form.Get("choice")}})
+		if resp.StatusCode == 605 {
+			fmt.Fprintf(w, friend + " does not exist")
+		} else {
+			http.Redirect(w, r, "/manage", 301)
+		}
+	}
+}
+
+// handler for deleting user account
+func deleteAccount(w http.ResponseWriter, r *http.Request) {
+	userCookie, _ := r.Cookie("SessionID")
+	if userCookie == nil {
+		http.Redirect(w, r, "/login", 301)
+	}
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("delete_account.html")
+		t.Execute(w, nil)
+	} else if r.Method == "POST" {
+		
+		// expire users cookie and remove them from User list, redirect to login page
+		username := userCookie.Value
+		cookieValue := username
+		expire := time.Now()
+		userCookie := http.Cookie{Name: "SessionID", Value: cookieValue, Expires: expire}
+		http.SetCookie(w, &userCookie)
+		
+		http.PostForm("http://localhost:8081/delete", url.Values{"username": {username}})
+
+		http.Redirect(w, r, "/login", 301)
+	}
+}
+
+
+// handler for logging out of site
+func logout(w http.ResponseWriter, r *http.Request) {
+	userCookie, _ := r.Cookie("SessionID")
+	if userCookie == nil {
+		http.Redirect(w, r, "/login", 301)
+	} 
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("logout.html")
+		t.Execute(w, nil)
+	} else if r.Method == "POST" {
+		
+		// expire cookie & redirect them back to login page
+		username := userCookie.Value
+		cookieValue := username
+		expire := time.Now()
+		userCookie := http.Cookie{Name: "SessionID", Value: cookieValue, Expires: expire}
+		http.SetCookie(w, &userCookie)
+		http.Redirect(w, r, "/login", 301)
+	}
+}
+
+// handler for making a new account
+func makeAccount(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET"  {
+		t, _ := template.ParseFiles("create_account.html")
+		t.Execute(w, nil)
+	} else if r.Method == "POST" {
+		r.ParseForm()
+		resp, _ := http.PostForm("http://localhost:8081/create", url.Values{"username": {r.PostFormValue("username")}, "name": {r.PostFormValue("name")}, "email": {r.PostFormValue("email")},"password": {r.PostFormValue("password")}})
+		if resp.StatusCode == 607 {
+			fmt.Fprintf(w, "Username already exists")
+		} else if resp.StatusCode == 606 {
+			fmt.Fprintf(w, "Invalid entry")
+		} else {
+			http.Redirect(w, r, "/login", 301)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/main", post_croak)
+	http.HandleFunc("/create", makeAccount)
+    http.HandleFunc("/login", login)
+    http.HandleFunc("/delete", deleteAccount)
+    http.HandleFunc("/manage", manageFriends)
+    http.ListenAndServe(":8080", nil)
+}
--- ribbit_server.go
+++ ribbit_server.go
@@ -1,3 +1,7 @@
+// Project Pt. 2
+// LHF238 / N18380086
+// Backend
+
 package main
 
 import (
@@ -5,8 +9,11 @@ import (
 	"net/http"
 	"log"
 	"fmt"
-	"html/template"
-	"time"
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"github.com/nu7hatch/gouuid"
+
 )
 
 // struct for a user object, where user data is stored ... (friend_list is map of all their friends by username)
@@ -30,153 +37,226 @@ var Users = make(map[string]user)
 var Posts []post
 var time_track int = 0
 
+
+// function to create user directory, with personal data file, and friends directory
+func createUserFile(username string, name string, password string, email string) {
+	os.Mkdir("Users/" + username, 0777)
+	os.Mkdir("Users/" + username + "/friends", 0777)
+	os.Mkdir("Users/" + username + "/posts", 0777)
+
+
+	// make user friends with self
+	self_file, self_err := os.Create("Users/" + username + "/friends/" + username + ".txt")
+	defer self_file.Close()
+
+	if self_err != nil {
+		log.Fatal(self_err)
+	}
+
+	// make file with name
+	name_file, name_err := os.Create("Users/" + username + "/name.txt")
+	defer name_file.Close()
+	if name_err != nil {
+		log.Fatal(name_err)
+	}
+
+	name_file.WriteString(name)
+	
+	// make file with password
+	password_file, password_err := os.Create("Users/" + username + "/password.txt")
+	defer password_file.Close()
+	if password_err != nil {
+		log.Fatal(password_err)
+	}
+
+	password_file.WriteString(password)
+
+	// make file with email
+	email_file, email_err := os.Create("Users/" + username + "/email.txt")
+	defer email_file.Close()
+	if email_err != nil {
+		log.Fatal(email_err)
+	}
+	
+	email_file.WriteString(email)
+
+}
+
+// function to delete user file
+func deleteUserFile(username string) {
+	err := os.RemoveAll("Users/" + username)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// function adds a file with friend's name into user's friend directory
+func addFriendForUser(username string, friend string) {
+	file, err := os.Create("Users/" + username + "/friends/" + friend + ".txt")
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// function removes a file with friend's name from user's friend directory
+func removeFriendForUser(username string, friend string) {
+	err := os.Remove("Users/" + username + "/friends/" + friend + ".txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func userExists(username string) bool {
+	if _, err := os.Stat("Users/" + username); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+
+// checks to see if password correct for the username
+func passwordCorrect(username string, password string) bool {
+	file, err := os.Open("Users/" + username + "/password.txt")
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	realPassword, _ := ioutil.ReadFile("Users/" + username + "/password.txt")
+	if string(realPassword) == password {
+		return true
+	}
+	return false
+}
+
+
+//add post to user's file
+func addPostFile(username string, post string) {
+	my_uuid, _ := uuid.NewV4()
+	file, err := os.Create("Users/" + username + "/posts/" + my_uuid.String() + ".txt")
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file.WriteString(post)
+
+}
+
+
+// func to check if users are friends
+func areFriends(username string, friend string) bool {
+	if _, err := os.Stat("Users/" + username + "/friends/" + friend + ".txt"); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
 // function for selecting posts for users based on their friends list
 func createUserFeed(username string) []post {
 	var user_feed []post
-	for k, _ := range Users[username].friend_list {
-		for i := 0; i < len(Posts); i++ {
-			if Posts[i].Username == k {
-				user_feed = append(user_feed, Posts[i])
+	friends, _ := filepath.Glob("Users/" + username + "/friends/*")
+	fmt.Println("friend list", friends)
+	for _, friend := range friends {
+		friend_name := friend[len("Users/" + username + "/friends/"):len(friend) - 4]
+		fmt.Println("friends name", friend_name)
+		friend_posts, _ := filepath.Glob("Users/" + friend_name + "/posts/*")
+		fmt.Println("friends posts", friend_posts)
+		for _, fpost := range friend_posts {
+			fmt.Println("file name", fpost)
+
+			// for mac OS, have to add this if statement to prevent opening .DS_Store file
+			if fpost != "Users/" + friend_name + "/posts/.DS_Store" {
+				new_post_string, _ := ioutil.ReadFile(fpost)
+				fmt.Println("new string", new_post_string)
+				var new_post post
+				new_post.Post_data = string(new_post_string[:])
+				new_post.Username = friend_name
+				new_post.Time_id = 0
+				user_feed = append(user_feed, new_post)
 			}
 		}
 	}
 	return user_feed
 }
 
+
+
 // handler for main feed page, to post and see other's posts
 func post_croak(w http.ResponseWriter, r *http.Request) {
-	userCookie, _ := r.Cookie("SessionID")
-	if userCookie == nil {
-		form, err := ioutil.ReadFile("ribbit_home_page.html")
-		if err != nil {
-				log.Fatal(err)
-			}
-		fmt.Fprintf(w, string(form))
-	} else {
-		switch r.Method {
-			case http.MethodPost:
-				r.ParseForm()
-				if len(r.PostFormValue("post")) > 100 {
-					fmt.Fprintf(w, "Post greater than 100 characters not allowed")
-				} else {
+	if r.Method == http.MethodPost {
+		fmt.Println("post req main")
+		r.ParseForm()
+		if len(r.PostFormValue("post_data")) > 100 {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), 512)	
+			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusInternalServerError)
 
-					// add users post to Post list
-					var userPost post
-					userPost.Post_data = r.PostFormValue("post")
-					userPost.Time_id = time_track
-					userPost.Username = userCookie.Value
-					time_track++
-					Posts = append(Posts, userPost)
-				}
+		} else {
+			
+			addPostFile(r.PostFormValue("username"), r.PostFormValue("post_data"))
+
 		}
-
-		// create custom feed for user based on friends & template it
-		user_feed := createUserFeed(userCookie.Value)
-		t := template.New("some_name")
-		t, _ = template.ParseFiles("main_feed.html")
-		t.Execute(w, user_feed)
 	}
 }
 
-
-// checks to see if user exists in Users map
-func userExists(username string) bool {
-	_, ok := Users[username]
-	return ok
-}
-
-// checks to see if password correct for the username
-func passwordCorrect(username string, password string) bool {
-	if (Users[username].password == password) {
-		return true
+func getUserFeed(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		user_feed := createUserFeed(r.PostFormValue("username"))
+		jsonFeed, _ := json.Marshal(user_feed)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonFeed) 
 	}
-	return false
 }
+
 
 // login handler
 func login(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("ribbit_home_page.html")
-		t.Execute(w, nil)
-	} else if r.Method == "POST" {
+	if r.Method == "POST" {
+		fmt.Println("post req login")
 		r.ParseForm()
 		username := r.PostFormValue("username")
 		password := r.PostFormValue("password")
-		//fmt.Println("username: ", username)
-		//fmt.Println("password: ", password)
 		
 		// make sure username is associated with an accpunt
 		if !userExists(username) {
-			fmt.Fprintf(w, "No account exists.")
+			http.Error(w, http.StatusText(http.StatusInternalServerError), 601)
+			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusInternalServerError)
 
 		// make sure password is correct
 		} else if !passwordCorrect(username, password) {
-			fmt.Fprintf(w, "Wrong Password")
-		} else {
-
-			// create cookie for user, and log them in by redirecting to /main
-			cookieValue := username
-			expire := time.Now().Add(1 * time.Hour)
-			userCookie := http.Cookie{Name: "SessionID", Value: cookieValue, Expires: expire}
-			http.SetCookie(w, &userCookie)
-			http.Redirect(w, r, "/main", 301)
-		}
+			http.Error(w, http.StatusText(http.StatusInternalServerError), 602)
+			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Println("password inc")
+		 }
 	}
 }
 
-// add friend to a users friend list
-func addFriendForUser(username string, friend string) {
-	Users[username].friend_list[friend] = true
-}
-
-// remove friend from a users friend list
-func removeFriendForUser(username string, friend string) {
-	delete(Users[username].friend_list, friend)
-}
-
-// check whether 'friend' is in 'usernames' friend list
-func areFriends(username string, friend string) bool {
-	_, ok := Users[username].friend_list[friend]
-	return ok
-}
 
 // handler for adding and removing friends
 func manageFriends(w http.ResponseWriter, r *http.Request) {
-	userCookie, _ := r.Cookie("SessionID")
-	if userCookie == nil {
-		form, err := ioutil.ReadFile("ribbit_home_page.html")
-		if err != nil {
-				log.Fatal(err)
-			}
-		fmt.Fprintf(w, string(form))
-	}
-	username := userCookie.Value
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("manage_friends.html")
-		t.Execute(w, nil)
-	} else if r.Method == "POST" {
+	if r.Method == "POST" {
+		fmt.Println("post req manage")
 		r.ParseForm()
-		friend := r.PostFormValue("username")
+		username := r.PostFormValue("username")
+		friend := r.PostFormValue("friend")
 		if userExists(friend) {
 			choice := r.Form.Get("choice") // are they adding or removing friend
-			if choice == "add" { 
-				if areFriends(username, friend) { // make sure they are not already friends
-					fmt.Fprintf(w, "user %s is already your friend!", friend)
-				} else if friend == username { // can't delete yourself as a friend
-					fmt.Fprintf(w, "%s if you!", friend)
-				} else  {
-					addFriendForUser(username, friend)
-				}
-			} else { // choice is to remove friend
-				if !areFriends(username, friend) {
-					fmt.Fprintf(w, "user %s was never your friend!", friend)
-				} else {
-					removeFriendForUser(username, friend)
-				}
+			if choice == "add" && !areFriends(username, friend) && friend != username { 
+				addFriendForUser(username, friend)
+			} else if choice == "remove" && areFriends(username, friend) && friend != username { // choice is to remove friend
+				removeFriendForUser(username, friend)
 			}
-			http.Redirect(w, r, "/manage", 301)
 		} else {
-			fmt.Fprintf(w, "user %s does not exist", friend)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), 605)
+			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusInternalServerError)
 		}
 	}
 }
@@ -185,96 +265,60 @@ func manageFriends(w http.ResponseWriter, r *http.Request) {
 // handler for deleting a user from the site
 func deleteUser(username string) {
 
-	// delete user from Users "database"
-	delete(Users, username)
 
-	// delete all posts the user made
-	for i := 0; i < len(Posts); i++ {
-		if Posts[i].Username == username {
-			Posts[len(Posts) - 1], Posts[i] = Posts[i], Posts[len(Posts) - 1]
-			Posts = Posts[:len(Posts) - 1]
-		}
-	}
+	// delete user from Users "database"
+	deleteUserFile(username)
+
 
 	// delete user from other user's friend lists
-	for k1, _ := range Users {
-		for k2, _ := range Users[k1].friend_list {
-			if k2 == username {
-				delete(Users[k1].friend_list, k2)
-			}
+	users, _ := filepath.Glob("Users/*")
+	for _, user := range users {
 
+		// for mac OS, have to add this if statement to prevent opening .DS_Store file
+		if user != "Users/.DS_Store/friends/l.txt" {
+			fmt.Println("user: ", user)
+			fmt.Println("USER PATH: ", user + "/friends/" + username + ".txt")
+			if _, err := os.Stat(user + "/friends/" + username + ".txt"); os.IsNotExist(err) {
+				break
+			} else {
+				e := os.RemoveAll(user + "/friends/" + username + ".txt")
+				if e != nil {
+					log.Fatal(e)
+				}
+			}
 		}
 	}
 }
+
 
 // handler for deleting user account
 func deleteAccount(w http.ResponseWriter, r *http.Request) {
-	userCookie, _ := r.Cookie("SessionID")
-	if userCookie == nil {
-		form, err := ioutil.ReadFile("ribbit_home_page.html")
-		if err != nil {
-				log.Fatal(err)
-			}
-		fmt.Fprintf(w, string(form))
-	}
-	//fmt.Println("username from cookie is: ", userCookie.Value)
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("delete_account.html")
-		t.Execute(w, nil)
-	} else if r.Method == "POST" {
-		
-		// expire users cookie and remove them from User list, redirect to login page
-		username := userCookie.Value
-		cookieValue := username
-		expire := time.Now()
-		userCookie := http.Cookie{Name: "SessionID", Value: cookieValue, Expires: expire}
-		http.SetCookie(w, &userCookie)
+	if r.Method == "POST" {
+		r.ParseForm()
+		username := r.PostFormValue("username")
 		deleteUser(username)
-		http.Redirect(w, r, "/login", 301)
+		fmt.Println("delete", r.PostFormValue("username"))
 	}
 }
 
 
-// handler for logging out of site
-func logout(w http.ResponseWriter, r *http.Request) {
-	userCookie, _ := r.Cookie("SessionID")
-	if userCookie == nil {
-		form, err := ioutil.ReadFile("ribbit_home_page.html")
-		if err != nil {
-				log.Fatal(err)
-			}
-		fmt.Fprintf(w, string(form))
-	} 
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("logout.html")
-		t.Execute(w, nil)
-	} else if r.Method == "POST" {
-		
-		// expire cookie & redirect them back to login page
-		username := userCookie.Value
-		cookieValue := username
-		expire := time.Now()
-		userCookie := http.Cookie{Name: "SessionID", Value: cookieValue, Expires: expire}
-		http.SetCookie(w, &userCookie)
-		http.Redirect(w, r, "/login", 301)
-	}
-}
 
 // handler for making a new account
 func makeAccount(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("create_account.html")
-		t.Execute(w, nil)
-	} else if r.Method == "POST" {
+	if r.Method == "POST" {
 		r.ParseForm()
 		username := r.PostFormValue("username")
 		name := r.PostFormValue("name")
 		email := r.PostFormValue("email")
 		password := r.PostFormValue("password")
+
+		fmt.Println("Username: ", username)
 		
 		// make sure all fields have an input value, not an empty string
 		if (username == "" || name == "" || email == "" || password == "") {
-			fmt.Fprintf(w, "you did not fill out form correctly")
+			http.Error(w, http.StatusText(http.StatusInternalServerError), 606)
+			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusInternalServerError)
 		} else {
 
 			// add new user to map Users, so long as the user does not already exist
@@ -285,22 +329,24 @@ func makeAccount(w http.ResponseWriter, r *http.Request) {
 			new_user.email = email
 			new_user.friend_list = make(map[string]bool)
 			new_user.friend_list[username] = true
-			if userExists(r.PostFormValue("username")) {
-				fmt.Fprintf(w, "Username %s already exists", username)
+			if userExists(username) {
+				http.Error(w, "UserNameExists", 607)
 			} else {
+				createUserFile(username, name, password, email)
+				
+				// delete line when done implimenting DB
 				Users[username] = new_user
-				http.Redirect(w, r, "/login", 301)
 			}
 		}
 	}
 }
 
 func main() {
-	http.HandleFunc("/logout", logout)
 	http.HandleFunc("/main", post_croak)
+	http.HandleFunc("/getUserFeed", getUserFeed)
 	http.HandleFunc("/create", makeAccount)
     http.HandleFunc("/login", login)
     http.HandleFunc("/delete", deleteAccount)
     http.HandleFunc("/manage", manageFriends)
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(":8081", nil)
 }
