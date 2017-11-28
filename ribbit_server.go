7f0e34ec8e6848aea3727a58f5259fcd54f0cc5a
Lisa Frankel <lisa@frankel.tv>
2017-11-28 11:33:12 -0500

    updated backend server
    communication between frontend and backend now using tcp, backend is multithreaded and uses locks to ensure thread safety

--- ribbit_server.go
+++ ribbit_server.go
@@ -1,39 +1,59 @@
 package main
 
 import (
-	"io/ioutil"
-	"net/http"
-	"log"
-	"fmt"
 	"encoding/json"
+	"fmt"
+	"github.com/nu7hatch/gouuid"
+	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
-	"github.com/nu7hatch/gouuid"
-
+	"net"
+	"strings"
+	"sync"
 )
 
-// struct for a user object, where user data is stored ... (friend_list is map of all their friends by username)
-type user struct {
-	username string
-	password string
-	email string
-	name string
-	friend_list map[string]bool
+var filelocks = make(map[string]*sync.RWMutex)
+
+
+// struct for a post object, where post data is stored
+type post struct {
+	Username  string
+	Post_data string
+	Time_id   int
 }
 
-// struct for a post object, where post data is stored 
-type post struct {
-	Username string
-	Post_data string
-	Time_id int
+func getLock(filename string) {
+	if _, ok := filelocks[filename]; ok {
+		filelocks[filename].Lock()
+		fmt.Println("locking: ", filename, "in getLock")
+	} else {
+		filelocks[filename] = &sync.RWMutex{}
+		fmt.Println("new lock, locking: ", filename, "in getLock")
+		filelocks[filename].Lock()
+	}
 }
+
+func getReadLock(filename string) {
+	if _, ok := filelocks[filename]; ok {
+		fmt.Println("locking: ", filename, "in getReadLock")
+		filelocks[filename].RLock()
+	} else {
+		filelocks[filename] = &sync.RWMutex{}
+		fmt.Println("new lock, locking: ", filename, "in getReadLock")
+		filelocks[filename].RLock()
+	}
+}
+
 
 // function to create user directory, with personal data file, and friends directory
 func createUserFile(username string, name string, password string, email string) {
-	os.Mkdir("Users/" + username, 0777)
-	os.Mkdir("Users/" + username + "/friends", 0777)
-	os.Mkdir("Users/" + username + "/posts", 0777)
+	os.Mkdir("Users/"+username, 0777)
+	os.Mkdir("Users/"+username+"/friends", 0777)
+	os.Mkdir("Users/"+username+"/posts", 0777)
 
+	filelocks["Users/"+username] = &sync.RWMutex{}
+	fmt.Println("creating lock: ", "Users/", username, "in create user file")
 
 	// make user friends with self
 	self_file, self_err := os.Create("Users/" + username + "/friends/" + username + ".txt")
@@ -51,7 +71,7 @@ func createUserFile(username string, name string, password string, email string)
 	}
 
 	name_file.WriteString(name)
-	
+
 	// make file with password
 	password_file, password_err := os.Create("Users/" + username + "/password.txt")
 	defer password_file.Close()
@@ -67,23 +87,29 @@ func createUserFile(username string, name string, password string, email string)
 	if email_err != nil {
 		log.Fatal(email_err)
 	}
-	
+
 	email_file.WriteString(email)
 
 }
 
 // function to delete user file
 func deleteUserFile(username string) {
+	getLock("Users/" + username)
 	err := os.RemoveAll("Users/" + username)
 	if err != nil {
 		log.Fatal(err)
 	}
+	filelocks["Users/" + username].Unlock()
+	fmt.Println("unlocked", "Users/" + username, "in delete user file")
 }
 
 // function adds a file with friend's name into user's friend directory
 func addFriendForUser(username string, friend string) {
+	getReadLock("Users/" + username)
 	file, err := os.Create("Users/" + username + "/friends/" + friend + ".txt")
 	defer file.Close()
+	filelocks["Users/" + username].RUnlock()
+	fmt.Println("read unlocking Users/", username)
 
 	if err != nil {
 		log.Fatal(err)
@@ -92,76 +118,107 @@ func addFriendForUser(username string, friend string) {
 
 // function removes a file with friend's name from user's friend directory
 func removeFriendForUser(username string, friend string) {
-	err := os.Remove("Users/" + username + "/friends/" + friend + ".txt")
+	filename := "Users/" + username + "/friends/" + friend + ".txt"
+	getLock(filename)
+	getReadLock("Users/" + username)
+	err := os.Remove(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
+	filelocks[filename].Unlock()
+	filelocks["Users/" + username].RUnlock()
+	fmt.Println("read unlocking Users/", username)
+	fmt.Println("unlocked", filename, "in removeFriendForUser")
 }
 
 func userExists(username string) bool {
+	getReadLock("Users/" + username)
 	if _, err := os.Stat("Users/" + username); os.IsNotExist(err) {
 		return false
 	}
+	filelocks["Users/" + username].RUnlock()
+	fmt.Println("read unlocking Users/", username)
 	return true
 }
 
-
 // checks to see if password correct for the username
 func passwordCorrect(username string, password string) bool {
-	file, err := os.Open("Users/" + username + "/password.txt")
+	filename := "Users/" + username + "/password.txt"
+	getReadLock("Users/" + username)
+	file, err := os.Open(filename)
 	defer file.Close()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	realPassword, _ := ioutil.ReadFile("Users/" + username + "/password.txt")
+	getReadLock(filename)
+	realPassword, _ := ioutil.ReadFile(filename)
+	filelocks[filename].RUnlock()
+	filelocks["Users/" + username].RUnlock()
+	fmt.Println("read unlocking Users/", username)
+	fmt.Println("unlocked", filename, "in passwordCorrect")
 	if string(realPassword) == password {
 		return true
 	}
 	return false
 }
 
-
 //add post to user's file
 func addPostFile(username string, post string) {
 	my_uuid, _ := uuid.NewV4()
-	file, err := os.Create("Users/" + username + "/posts/" + my_uuid.String() + ".txt")
+	filename := "Users/" + username + "/posts/" + my_uuid.String() + ".txt"
+	getReadLock("Users/" + username)
+	file, err := os.Create(filename)
 	defer file.Close()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	getLock(filename)
 	file.WriteString(post)
+	
+	filelocks[filename].Unlock()
+	filelocks["Users/" + username].RUnlock()
+	fmt.Println("read unlocking Users/", username)
+	fmt.Println("unlocked", filename, "in addPostFile")
 
 }
-
 
 // func to check if users are friends
 func areFriends(username string, friend string) bool {
+	getReadLock("Users/" + username)
 	if _, err := os.Stat("Users/" + username + "/friends/" + friend + ".txt"); os.IsNotExist(err) {
 		return false
 	}
+	filelocks["Users/" + username].RUnlock()
+	fmt.Println("read unlocking Users/", username)
 	return true
 }
+
 
 // function for selecting posts for users based on their friends list
 func createUserFeed(username string) []post {
 	var user_feed []post
+	getReadLock("Users/" + username)
 	friends, _ := filepath.Glob("Users/" + username + "/friends/*")
 	fmt.Println("friend list", friends)
 	for _, friend := range friends {
-		friend_name := friend[len("Users/" + username + "/friends/"):len(friend) - 4]
+		friend_name := friend[len("Users/"+username+"/friends/") : len(friend)-4]
 		fmt.Println("friends name", friend_name)
+		getReadLock("Users/"+friend_name)
 		friend_posts, _ := filepath.Glob("Users/" + friend_name + "/posts/*")
 		fmt.Println("friends posts", friend_posts)
 		for _, fpost := range friend_posts {
 			fmt.Println("file name", fpost)
 
 			// for mac OS, have to add this if statement to prevent opening .DS_Store file
-			if fpost != "Users/" + friend_name + "/posts/.DS_Store" {
+			if fpost != "Users/"+friend_name+"/posts/.DS_Store" {
+				getReadLock(fpost)
 				new_post_string, _ := ioutil.ReadFile(fpost)
+				filelocks[fpost].RUnlock()
+				fmt.Println("unlocked", fpost, "in createUserFeed")
 				fmt.Println("new string", new_post_string)
 				var new_post post
 				new_post.Post_data = string(new_post_string[:])
@@ -170,171 +227,185 @@ func createUserFeed(username string) []post {
 				user_feed = append(user_feed, new_post)
 			}
 		}
+		filelocks["Users/"+friend_name].RUnlock()
 	}
+	filelocks["Users/" + username].RUnlock()
+	fmt.Println("read unlocking Users/", username)
 	return user_feed
 }
 
-
-
 // handler for main feed page, to post and see other's posts
-func post_croak(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		fmt.Println("post req main")
-		r.ParseForm()
-		if len(r.PostFormValue("post_data")) > 100 {
-			http.Error(w, http.StatusText(http.StatusInternalServerError), 512)	
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(http.StatusInternalServerError)
-
-		} else {
-			
-			addPostFile(r.PostFormValue("username"), r.PostFormValue("post_data"))
-
-		}
+func post_croak(post string, username string) string {
+	if len(post) > 100 {
+		return "512"
+	} else {
+		addPostFile(username, post)
+		return "200"
 	}
 }
 
-func getUserFeed(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		user_feed := createUserFeed(r.PostFormValue("username"))
-		jsonFeed, _ := json.Marshal(user_feed)
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(jsonFeed) 
-	}
+func getUserFeed(username string) []byte {
+	user_feed := createUserFeed(username)
+	jsonFeed, _ := json.Marshal(user_feed)
+	return jsonFeed
 }
-
 
 // login handler
-func login(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		fmt.Println("post req login")
-		r.ParseForm()
-		username := r.PostFormValue("username")
-		password := r.PostFormValue("password")
-		
-		// make sure username is associated with an accpunt
-		if !userExists(username) {
-			http.Error(w, http.StatusText(http.StatusInternalServerError), 601)
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(http.StatusInternalServerError)
-
-		// make sure password is correct
-		} else if !passwordCorrect(username, password) {
-			http.Error(w, http.StatusText(http.StatusInternalServerError), 602)
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Println("password inc")
-		 }
+func login(username string, password string) string {
+	// make sure username is associated with an account & sure password is correct
+	if !userExists(username) {
+		return "601"
+	} else if !passwordCorrect(username, password) {
+		return "602"
 	}
+	return "200"
 }
-
 
 // handler for adding and removing friends
-func manageFriends(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		fmt.Println("post req manage")
-		r.ParseForm()
-		username := r.PostFormValue("username")
-		friend := r.PostFormValue("friend")
-		if userExists(friend) {
-			choice := r.Form.Get("choice") // are they adding or removing friend
-			if choice == "add" && !areFriends(username, friend) && friend != username { 
-				addFriendForUser(username, friend)
-			} else if choice == "remove" && areFriends(username, friend) && friend != username { // choice is to remove friend
-				removeFriendForUser(username, friend)
-			}
-		} else {
-			http.Error(w, http.StatusText(http.StatusInternalServerError), 605)
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(http.StatusInternalServerError)
+func manageFriends(username string, friend string, choice string) string {
+	if userExists(friend) {
+		if choice == "add" && !areFriends(username, friend) && friend != username {
+			addFriendForUser(username, friend)
+		} else if choice == "remove" && areFriends(username, friend) && friend != username { // choice is to remove friend
+			removeFriendForUser(username, friend)
 		}
+		return "200"
+	} else {
+		return "601"
 	}
 }
-
 
 // handler for deleting a user from the site
 func deleteUser(username string) {
 
-
 	// delete user from Users "database"
 	deleteUserFile(username)
-
 
 	// delete user from other user's friend lists
 	users, _ := filepath.Glob("Users/*")
 	for _, user := range users {
-
 		// for mac OS, have to add this if statement to prevent opening .DS_Store file
-		if user != "Users/.DS_Store/friends/l.txt" {
-			fmt.Println("user: ", user)
-			fmt.Println("USER PATH: ", user + "/friends/" + username + ".txt")
+		if user != "Users/.DS_Store/friends/"+username+".txt" {
 			if _, err := os.Stat(user + "/friends/" + username + ".txt"); os.IsNotExist(err) {
 				break
 			} else {
-				e := os.RemoveAll(user + "/friends/" + username + ".txt")
+				getReadLock("Users/"+user)
+				filename := user + "/friends/" + username + ".txt"
+				getLock(filename)
+				e := os.RemoveAll(filename)
 				if e != nil {
 					log.Fatal(e)
 				}
+				filelocks[filename].Unlock()
+				filelocks["Users/"+user].RUnlock()
+				fmt.Println("unlocked", filename, "in deleteUser")
 			}
 		}
 	}
 }
-
 
 // handler for deleting user account
-func deleteAccount(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		r.ParseForm()
-		username := r.PostFormValue("username")
-		deleteUser(username)
-		fmt.Println("delete", r.PostFormValue("username"))
-	}
+func deleteAccount(username string) {
+	deleteUser(username)
+	fmt.Println("delete", username)
 }
 
-
-
 // handler for making a new account
-func makeAccount(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		r.ParseForm()
-		username := r.PostFormValue("username")
-		name := r.PostFormValue("name")
-		email := r.PostFormValue("email")
-		password := r.PostFormValue("password")
-
-		fmt.Println("Username: ", username)
-		
-		// make sure all fields have an input value, not an empty string
-		if (username == "" || name == "" || email == "" || password == "") {
-			http.Error(w, http.StatusText(http.StatusInternalServerError), 606)
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(http.StatusInternalServerError)
+func makeAccount(username string, name string, email string, password string) string {
+	fmt.Println("in server side make accounts")
+	if username == "" || name == "" || email == "" || password == "" {
+		return "606"
+	} else {
+		if userExists(username) {
+			return "607"
 		} else {
-
-			// add new user to map Users, so long as the user does not already exist
-			var new_user user
-			new_user.name = name
-			new_user.username = username
-			new_user.password = password
-			new_user.email = email
-			new_user.friend_list = make(map[string]bool)
-			new_user.friend_list[username] = true
-			if userExists(username) {
-				http.Error(w, "UserNameExists", 607)
-			} else {
-				createUserFile(username, name, password, email)
-				
-			}
+			createUserFile(username, name, password, email)
 		}
 	}
+
+	return "200"
+}
+
+func clientConns(listener net.Listener) chan net.Conn {
+	clientJobs := make(chan net.Conn)
+	go func () {
+		for {
+			client, err := listener.Accept()
+			if err != nil {
+				fmt.Println(err)
+			}
+			clientJobs <- client
+		}
+	} ()
+	return clientJobs
+}
+
+func handleConn(client net.Conn) {
+	fmt.Println("in handle conn server func")	
+	defer client.Close()
+	// read message from client
+	message := make([]byte, 1024)
+	n, err := client.Read(message)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// format message into tokens
+	s_message := string(message[:n])
+	fmt.Println(s_message)
+	message_tokens := strings.Split(s_message, ",")
+	if message_tokens[0] == "create" {
+		fmt.Println("create request")
+		statusCode := makeAccount(message_tokens[1], message_tokens[2], message_tokens[3], message_tokens[4])
+		fmt.Fprintf(client, statusCode)
+		return
+	}
+	if message_tokens[0] == "delete" {
+		fmt.Println("delete request")
+		deleteAccount(message_tokens[1])
+		return
+	}
+	if message_tokens[0] == "manage" {
+		fmt.Println("manage request")
+		statusCode := manageFriends(message_tokens[1], message_tokens[2], message_tokens[3])
+		fmt.Fprintf(client, statusCode)
+		return
+	}
+	if message_tokens[0] == "login" {
+		fmt.Println("login request")
+		statusCode := login(message_tokens[1], message_tokens[2])
+		fmt.Fprintf(client, statusCode)
+		return
+	}
+	if message_tokens[0] == "post" {
+		fmt.Println("post request")
+		statusCode := post_croak(message_tokens[1], message_tokens[2])
+		fmt.Fprintf(client, statusCode)
+		return
+	}
+	if message_tokens[0] == "getUserFeed" {
+		fmt.Println("get user feed request")
+		jsonFeed := getUserFeed(message_tokens[1])
+		// fmt.Fprintf(client, "jsonFeed")
+		client.Write(jsonFeed)
+		return
+	}
+	
 }
 
 func main() {
-	http.HandleFunc("/main", post_croak)
-	http.HandleFunc("/getUserFeed", getUserFeed)
-	http.HandleFunc("/create", makeAccount)
-    http.HandleFunc("/login", login)
-    http.HandleFunc("/delete", deleteAccount)
-    http.HandleFunc("/manage", manageFriends)
-    http.ListenAndServe(":8081", nil)
+	// create Users directory where users are stored
+	if _, err := os.Stat("Users/"); os.IsNotExist(err) {
+		os.Mkdir("Users/", 0777)
+	}
+	ln, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		fmt.Println(err)
+	}
+	conns := clientConns(ln)
+	for {
+		go handleConn(<-conns)
+	}
+
+
 }
