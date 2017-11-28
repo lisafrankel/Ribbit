f357362def0a282bf7699334628bb2d23cc40fcf
Lisa Frankel <lisa@frankel.tv>
2017-11-28 11:34:22 -0500

    updated frontend
    frontend now communicates with backend using tcp

--- client.go
+++ client.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"net/http"
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"net/http"
 	"time"
-	"encoding/json"
-	"net/url"
-	"log"
+	"net"
+	"bytes"
 )
 
 type post struct {
-	Username string
+	Username  string
 	Post_data string
-	Time_id int
+	Time_id   int
 }
 
 // handler for main feed page, to post and see other's posts
@@ -22,27 +22,47 @@ func post_croak(w http.ResponseWriter, r *http.Request) {
 	if userCookie == nil {
 		http.Redirect(w, r, "/login", 301)
 		return
-	} else if r.Method == http.MethodPost {
+	}
+	if r.Method == http.MethodPost {
 		r.ParseForm()
-		resp, _ := http.PostForm("http://localhost:8081/main", url.Values{"post_data": {r.PostFormValue("post")},"username": {userCookie.Value}})
-		if resp.StatusCode == 512 {
-			fmt.Fprintf(w, "post greater than 100 characters") //how do receive specific error from server?
-			return
+		post := r.PostFormValue("post")
+		username := userCookie.Value
+
+		conn, _ := net.Dial("tcp", "localhost:8081")
+		client_msg := "post," + post + "," + username
+		fmt.Fprintf(conn, client_msg)
+
+		response := make([]byte, 1024)
+		n, err := conn.Read(response)
+		if err != nil {
+			fmt.Println(err)
+			}
+		s_message := string(response[:n])
+
+		if s_message == "512" {
+			fmt.Fprintf(w, "post greater than 100 characters")
 		}
+		conn.Close()
 	}
 
-	// response here is []posts - userfeed // this changes to /getFeed
-	response, err := http.PostForm("http://localhost:8081/getUserFeed", url.Values {"username": {userCookie.Value}})
+	conn1, _ := net.Dial("tcp", "localhost:8081")
+	defer conn1.Close()
+	client_msg := "getUserFeed," + userCookie.Value
+	fmt.Fprintf(conn1, client_msg)
+
+	response := make([]byte, 1024)
+	_, err := conn1.Read(response)
 	if err != nil {
-		log.Fatal(err)
-	}
-	
-	dec := json.NewDecoder(response.Body)
+		fmt.Println(err)
+		}
+
+	reader := bytes.NewReader(response)
+	dec := json.NewDecoder(reader)
 	var user_feed []post
 	dec_err := dec.Decode(&user_feed)
-	
+
 	if dec_err != nil {
-		log.Fatal(dec_err)
+		fmt.Println(dec_err)
 	}
 
 	t := template.New("some_name")
@@ -50,18 +70,38 @@ func post_croak(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, user_feed)
 }
 
-
 // login handler
 func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("in login func")
+	userCookie, _ := r.Cookie("SessionID")
+	if userCookie != nil {
+		http.Redirect(w, r, "/main", 301)
+		return
+	}
 	if r.Method == "GET" {
 		t, _ := template.ParseFiles("ribbit_home_page.html")
 		t.Execute(w, nil)
 	} else if r.Method == "POST" {
 		r.ParseForm()
-		resp, _ := http.PostForm("http://localhost:8081/login", url.Values{"username": {r.PostFormValue("username")}, "password":{r.PostFormValue("password")}})
-		if resp.StatusCode == 601 {
+		username := r.PostFormValue("username")
+		password := r.PostFormValue("password")	
+
+
+		conn, _ := net.Dial("tcp", "localhost:8081")
+		defer conn.Close()
+		client_msg := "login," + username +"," + password
+		fmt.Fprintf(conn, client_msg)
+		
+		response := make([]byte, 1024)
+		n, err := conn.Read(response)
+		if err != nil {
+			fmt.Println(err)
+			}
+		s_message := string(response[:n])
+
+		if s_message == "601" {
 			fmt.Fprintf(w, "User does not exist")
-		} else if resp.StatusCode == 602 {
+		} else if s_message == "602" {
 			fmt.Fprintf(w, "Incorrect password")
 		} else {
 			cookieValue := r.PostFormValue("username")
@@ -78,6 +118,7 @@ func manageFriends(w http.ResponseWriter, r *http.Request) {
 	userCookie, _ := r.Cookie("SessionID")
 	if userCookie == nil {
 		http.Redirect(w, r, "/login", 301)
+		return
 	}
 	username := userCookie.Value
 	if r.Method == "GET" {
@@ -86,8 +127,21 @@ func manageFriends(w http.ResponseWriter, r *http.Request) {
 	} else if r.Method == "POST" {
 		r.ParseForm()
 		friend := r.PostFormValue("username")
-		resp, _ := http.PostForm("http://localhost:8081/manage", url.Values{"username": {username}, "friend": {friend}, "choice": {r.Form.Get("choice")}})
-		if resp.StatusCode == 605 {
+		choice := r.Form.Get("choice")
+
+		conn, _ := net.Dial("tcp", "localhost:8081")
+		defer conn.Close()
+		client_msg := "manage," + username + "," + friend + "," + choice
+		fmt.Fprintf(conn, client_msg)
+		
+		response := make([]byte, 1024)
+		n, err := conn.Read(response)
+		if err != nil {
+			fmt.Println(err)
+			}
+		s_message := string(response[:n])
+		
+		if s_message == "601" {
 			fmt.Fprintf(w, friend + " does not exist")
 		} else {
 			http.Redirect(w, r, "/manage", 301)
@@ -100,37 +154,42 @@ func deleteAccount(w http.ResponseWriter, r *http.Request) {
 	userCookie, _ := r.Cookie("SessionID")
 	if userCookie == nil {
 		http.Redirect(w, r, "/login", 301)
+		return
 	}
 	if r.Method == "GET" {
 		t, _ := template.ParseFiles("delete_account.html")
 		t.Execute(w, nil)
 	} else if r.Method == "POST" {
-		
+
 		// expire users cookie and remove them from User list, redirect to login page
 		username := userCookie.Value
 		cookieValue := username
 		expire := time.Now()
 		userCookie := http.Cookie{Name: "SessionID", Value: cookieValue, Expires: expire}
 		http.SetCookie(w, &userCookie)
-		
-		http.PostForm("http://localhost:8081/delete", url.Values{"username": {username}})
+
+		// tell server to delete username
+		conn, _ := net.Dial("tcp", "localhost:8081")
+		defer conn.Close()
+		client_msg := "delete," + username
+		fmt.Fprintf(conn, client_msg)
 
 		http.Redirect(w, r, "/login", 301)
 	}
 }
-
 
 // handler for logging out of site
 func logout(w http.ResponseWriter, r *http.Request) {
 	userCookie, _ := r.Cookie("SessionID")
 	if userCookie == nil {
 		http.Redirect(w, r, "/login", 301)
-	} 
+		return
+	}
 	if r.Method == "GET" {
 		t, _ := template.ParseFiles("logout.html")
 		t.Execute(w, nil)
 	} else if r.Method == "POST" {
-		
+
 		// expire cookie & redirect them back to login page
 		username := userCookie.Value
 		cookieValue := username
@@ -143,19 +202,48 @@ func logout(w http.ResponseWriter, r *http.Request) {
 
 // handler for making a new account
 func makeAccount(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET"  {
+	userCookie, _ := r.Cookie("SessionID")
+	if userCookie != nil {
+		http.Redirect(w, r, "/main", 301)
+		return
+	}
+	if r.Method == "GET" {
 		t, _ := template.ParseFiles("create_account.html")
 		t.Execute(w, nil)
 	} else if r.Method == "POST" {
 		r.ParseForm()
-		resp, _ := http.PostForm("http://localhost:8081/create", url.Values{"username": {r.PostFormValue("username")}, "name": {r.PostFormValue("name")}, "email": {r.PostFormValue("email")},"password": {r.PostFormValue("password")}})
-		if resp.StatusCode == 607 {
-			fmt.Fprintf(w, "Username already exists")
-		} else if resp.StatusCode == 606 {
-			fmt.Fprintf(w, "Invalid entry")
-		} else {
-			http.Redirect(w, r, "/login", 301)
+		
+		// connect to server & define variables that will be sent to server	
+		conn, _ := net.Dial("tcp", "localhost:8081")
+		defer conn.Close()
+		username := r.PostFormValue("username")
+		name := r.PostFormValue("name")
+		email := r.PostFormValue("email")
+		password := r.PostFormValue("password")
+		
+		// compose message to client, of request, username, name, email & password
+		client_msg := "create," + username + "," + name + "," + email + "," + password
+		
+		// send message to server
+		fmt.Fprintf(conn, client_msg)
+		//_, err := bufio.NewReader(conn).ReadString('\n')
+		response := make([]byte, 1024)
+		n, err := conn.Read(response)
+		if err != nil {
+			fmt.Println(err)
 		}
+
+		s_message := string(response[:n])
+		if s_message == "200" {
+			http.Redirect(w, r, "/login", 301)
+			return
+		}
+
+		if s_message == "607" {
+			fmt.Fprintf(w, "Username already exists")
+		} else if s_message == "606" {
+			fmt.Fprintf(w, "Invalid entry")
+		} 
 	}
 }
 
@@ -163,8 +251,8 @@ func main() {
 	http.HandleFunc("/logout", logout)
 	http.HandleFunc("/main", post_croak)
 	http.HandleFunc("/create", makeAccount)
-    http.HandleFunc("/login", login)
-    http.HandleFunc("/delete", deleteAccount)
-    http.HandleFunc("/manage", manageFriends)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/delete", deleteAccount)
+	http.HandleFunc("/manage", manageFriends)
+	http.ListenAndServe(":8080", nil)
 }
