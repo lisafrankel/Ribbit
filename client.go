6dcc7dbacb527d73fd29c14b04fe2a328cedf485
Lisa Frankel <lisa@frankel.tv>
2018-01-08 13:42:54 -0500

    Update client.go

--- client.go
+++ client.go
@@ -1,3 +1,7 @@
+// Author: Lisa Frankel
+// Client server
+
+
 package main
 
 import (
@@ -8,6 +12,7 @@ import (
 	"time"
 	"net"
 	"bytes"
+	"strings"
 )
 
 type post struct {
@@ -16,11 +21,85 @@ type post struct {
 	Time_id   int
 }
 
+// check errors
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+// function returns which server the client should READ from
+func getReadServer() string {
+	conn, _ := net.Dial("tcp", "localhost:8084") // load balancer
+	defer conn.Close()
+	fmt.Fprintf(conn, "read")
+
+	response := make([]byte, 1024)
+	n, err := conn.Read(response)
+	check(err)
+	port := string(response[:n])
+
+	fmt.Println("connecting to server: ", port)
+
+	return port
+}
+
+// function returns which server the client should write to (clients write to all active servers)
+func getWriteServer(w http.ResponseWriter) []string {
+	conn, _ := net.Dial("tcp", "localhost:8084") // load balancer
+	defer conn.Close()
+	fmt.Fprintf(conn, "write")
+
+	message := make([]byte, 1024)
+	n, err := conn.Read(message)
+	check(err)
+
+	// format message into tokens
+	s_message := string(message[:n])
+	fmt.Println(s_message)
+
+	if s_message == "nil,nil,nil" {
+		var servers []string
+		return servers
+	}
+
+	message_tokens := strings.Split(s_message, ",")
+
+	var servers []string
+	
+	for i := 0; i < len(message_tokens); i++ {
+		servers = append(servers, message_tokens[i])
+	}
+
+	return servers
+}
+
+// tell balancer a server is not responding
+func tellBalancer(port string) {
+	conn := writeToServer("8084", "down," + port)
+	conn.Close()
+}
+
+// writes client_msg to the server that listening on port
+func writeToServer(port string, client_msg string) net.Conn {
+	conn, err := net.Dial("tcp", "localhost:" + port)
+
+	if err != nil {
+		tellBalancer(port)
+		return nil
+	}
+
+	fmt.Fprintf(conn, client_msg)
+
+	return conn
+
+}
+
 // handler for main feed page, to post and see other's posts
 func post_croak(w http.ResponseWriter, r *http.Request) {
 	userCookie, _ := r.Cookie("SessionID")
 	if userCookie == nil {
-		http.Redirect(w, r, "/login", 301)
+		http.Redirect(w, r, "/login", 302)
 		return
 	}
 	if r.Method == http.MethodPost {
@@ -28,42 +107,65 @@ func post_croak(w http.ResponseWriter, r *http.Request) {
 		post := r.PostFormValue("post")
 		username := userCookie.Value
 
-		conn, _ := net.Dial("tcp", "localhost:8081")
 		client_msg := "post," + post + "," + username
-		fmt.Fprintf(conn, client_msg)
+
+		// Since posting does a WRITE, send message to all three servers.  However, only read response from 1.
+		var conn net.Conn
+
+		servers := getWriteServer(w)
+		if len(servers) == 0 {
+			fmt.Fprintf(w, "all servers are down")
+			return
+		}
+		for i := 0; i < len(servers); i++ {
+			if servers[i] != "nil" { // if server isn't down
+				conn1 := writeToServer(servers[i], client_msg)
+				if conn1 != nil { // if we discover that server crashed
+					conn = conn1
+				}
+			}
+		}
+
 
 		response := make([]byte, 1024)
 		n, err := conn.Read(response)
-		if err != nil {
-			fmt.Println(err)
-			}
+		check(err)
 		s_message := string(response[:n])
 
 		if s_message == "512" {
 			fmt.Fprintf(w, "post greater than 100 characters")
 		}
+
 		conn.Close()
 	}
 
-	conn1, _ := net.Dial("tcp", "localhost:8081")
-	defer conn1.Close()
+	// get server, since we are doing a read here
+	port := getReadServer()
+	conn, err := net.Dial("tcp", "localhost:" + port)
+	for err != nil {
+		tellBalancer(port)
+		port = getReadServer()
+		if port == "nil" {
+			fmt.Fprintf(w, "All servers are down")
+			return
+		}
+		conn, err = net.Dial("tcp", "localhost:" + port)
+	}
+
+	defer conn.Close()
 	client_msg := "getUserFeed," + userCookie.Value
-	fmt.Fprintf(conn1, client_msg)
+	fmt.Fprintf(conn, client_msg)
 
 	response := make([]byte, 1024)
-	_, err := conn1.Read(response)
-	if err != nil {
-		fmt.Println(err)
-		}
+	_, err1 := conn.Read(response)
+	check(err1)
 
 	reader := bytes.NewReader(response)
 	dec := json.NewDecoder(reader)
 	var user_feed []post
 	dec_err := dec.Decode(&user_feed)
 
-	if dec_err != nil {
-		fmt.Println(dec_err)
-	}
+	check(dec_err)
 
 	t := template.New("some_name")
 	t, _ = template.ParseFiles("main_feed.html")
@@ -75,7 +177,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("in login func")
 	userCookie, _ := r.Cookie("SessionID")
 	if userCookie != nil {
-		http.Redirect(w, r, "/main", 301)
+		http.Redirect(w, r, "/main", 302)
 		return
 	}
 	if r.Method == "GET" {
@@ -86,17 +188,31 @@ func login(w http.ResponseWriter, r *http.Request) {
 		username := r.PostFormValue("username")
 		password := r.PostFormValue("password")	
 
+		if username == "" || password == "" {
+			fmt.Fprintf(w, "Either username or password field blank")
+			http.Redirect(w, r, "/main", 302)
+			return
+		}
 
-		conn, _ := net.Dial("tcp", "localhost:8081")
+		// use getReadServer since we are only doing a read here
+		port := getReadServer()
+		conn, err := net.Dial("tcp", "localhost:" + port)
+		for err != nil {
+			tellBalancer(port)
+			port = getReadServer()
+			if port == "nil" {
+				fmt.Fprintf(w, "All servers are down")
+				return
+			}
+			conn, err = net.Dial("tcp", "localhost:" + port)
+		}	
 		defer conn.Close()
 		client_msg := "login," + username +"," + password
 		fmt.Fprintf(conn, client_msg)
 		
 		response := make([]byte, 1024)
 		n, err := conn.Read(response)
-		if err != nil {
-			fmt.Println(err)
-			}
+		check(err)
 		s_message := string(response[:n])
 
 		if s_message == "601" {
@@ -108,7 +224,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 			expire := time.Now().Add(1 * time.Hour)
 			userCookie := http.Cookie{Name: "SessionID", Value: cookieValue, Expires: expire}
 			http.SetCookie(w, &userCookie)
-			http.Redirect(w, r, "/main", 301)
+			http.Redirect(w, r, "/main", 302)
 		}
 	}
 }
@@ -117,7 +233,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 func manageFriends(w http.ResponseWriter, r *http.Request) {
 	userCookie, _ := r.Cookie("SessionID")
 	if userCookie == nil {
-		http.Redirect(w, r, "/login", 301)
+		http.Redirect(w, r, "/login", 302)
 		return
 	}
 	username := userCookie.Value
@@ -129,22 +245,33 @@ func manageFriends(w http.ResponseWriter, r *http.Request) {
 		friend := r.PostFormValue("username")
 		choice := r.Form.Get("choice")
 
-		conn, _ := net.Dial("tcp", "localhost:8081")
-		defer conn.Close()
 		client_msg := "manage," + username + "," + friend + "," + choice
-		fmt.Fprintf(conn, client_msg)
+
+		var conn net.Conn
+
+		servers := getWriteServer(w)
+		if len(servers) == 0 {
+			fmt.Fprintf(w, "all servers are down")
+			return
+		}
+		for i := 0; i < len(servers); i++ {
+			if servers[i] != "nil" {
+				conn1 := writeToServer(servers[i], client_msg)
+				if conn1 != nil { // if that server crashed
+					conn = conn1
+				}
+			}
+		}
 		
 		response := make([]byte, 1024)
 		n, err := conn.Read(response)
-		if err != nil {
-			fmt.Println(err)
-			}
+		check(err)
 		s_message := string(response[:n])
 		
 		if s_message == "601" {
 			fmt.Fprintf(w, friend + " does not exist")
 		} else {
-			http.Redirect(w, r, "/manage", 301)
+			http.Redirect(w, r, "/manage", 302)
 		}
 	}
 }
@@ -153,7 +280,7 @@ func manageFriends(w http.ResponseWriter, r *http.Request) {
 func deleteAccount(w http.ResponseWriter, r *http.Request) {
 	userCookie, _ := r.Cookie("SessionID")
 	if userCookie == nil {
-		http.Redirect(w, r, "/login", 301)
+		http.Redirect(w, r, "/login", 302)
 		return
 	}
 	if r.Method == "GET" {
@@ -169,12 +296,28 @@ func deleteAccount(w http.ResponseWriter, r *http.Request) {
 		http.SetCookie(w, &userCookie)
 
 		// tell server to delete username
-		conn, _ := net.Dial("tcp", "localhost:8081")
-		defer conn.Close()
-		client_msg := "delete," + username
-		fmt.Fprintf(conn, client_msg)
+		// send message to all 3 servers since we are doing a write
 
-		http.Redirect(w, r, "/login", 301)
+		client_msg := "delete," + username
+
+		var conn net.Conn
+
+		servers := getWriteServer(w)
+		if len(servers) == 0 {
+			fmt.Fprintf(w, "all servers are down")
+			return
+		}
+		for i := 0; i < len(servers); i++ {
+			if servers[i] != "nil" { // if server isn't down
+				conn1 := writeToServer(servers[i], client_msg)
+				if conn1 != nil { // if we discover that server crashed
+					conn = conn1
+				}
+			}
+		}
+
+		http.Redirect(w, r, "/login", 302)
+		conn.Close()
 	}
 }
 
@@ -182,7 +325,7 @@ func deleteAccount(w http.ResponseWriter, r *http.Request) {
 func logout(w http.ResponseWriter, r *http.Request) {
 	userCookie, _ := r.Cookie("SessionID")
 	if userCookie == nil {
-		http.Redirect(w, r, "/login", 301)
+		http.Redirect(w, r, "/login", 302)
 		return
 	}
 	if r.Method == "GET" {
@@ -196,7 +339,7 @@ func logout(w http.ResponseWriter, r *http.Request) {
 		expire := time.Now()
 		userCookie := http.Cookie{Name: "SessionID", Value: cookieValue, Expires: expire}
 		http.SetCookie(w, &userCookie)
-		http.Redirect(w, r, "/login", 301)
+		http.Redirect(w, r, "/login", 302)
 	}
 }
 
@@ -204,7 +347,7 @@ func logout(w http.ResponseWriter, r *http.Request) {
 func makeAccount(w http.ResponseWriter, r *http.Request) {
 	userCookie, _ := r.Cookie("SessionID")
 	if userCookie != nil {
-		http.Redirect(w, r, "/main", 301)
+		http.Redirect(w, r, "/main", 302)
 		return
 	}
 	if r.Method == "GET" {
@@ -213,9 +356,6 @@ func makeAccount(w http.ResponseWriter, r *http.Request) {
 	} else if r.Method == "POST" {
 		r.ParseForm()
 		
-		// connect to server & define variables that will be sent to server	
-		conn, _ := net.Dial("tcp", "localhost:8081")
-		defer conn.Close()
 		username := r.PostFormValue("username")
 		name := r.PostFormValue("name")
 		email := r.PostFormValue("email")
@@ -224,18 +364,32 @@ func makeAccount(w http.ResponseWriter, r *http.Request) {
 		// compose message to client, of request, username, name, email & password
 		client_msg := "create," + username + "," + name + "," + email + "," + password
 		
+		// Connect to all servers to do the write
+		var conn net.Conn
+
+		servers := getWriteServer(w)
+		if len(servers) == 0 {
+			fmt.Fprintf(w, "all servers are down")
+			return
+		}
+		for i := 0; i < len(servers); i++ {
+			if servers[i] != "nil" { // if server isn't down
+				conn1 := writeToServer(servers[i], client_msg)
+				if conn1 != nil { // if we discover that server crashed
+					conn = conn1
+				}
+			}
+		}
+
+
 		// send message to server
-		fmt.Fprintf(conn, client_msg)
-		//_, err := bufio.NewReader(conn).ReadString('\n')
 		response := make([]byte, 1024)
 		n, err := conn.Read(response)
-		if err != nil {
-			fmt.Println(err)
-		}
+		check(err)
 
 		s_message := string(response[:n])
 		if s_message == "200" {
-			http.Redirect(w, r, "/login", 301)
+			http.Redirect(w, r, "/login", 302)
 			return
 		}
 
@@ -244,6 +398,8 @@ func makeAccount(w http.ResponseWriter, r *http.Request) {
 		} else if s_message == "606" {
 			fmt.Fprintf(w, "Invalid entry")
 		} 
+
+		conn.Close()
 	}
 }
 
